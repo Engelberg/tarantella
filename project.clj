e7722a0759ef6c423ecb931311230c600d29e518
Mark Engelberg <mark.engelberg@gmail.com>
2016-06-28 07:05:22 -0700

    removed dependency on better-cond

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "http://github.com/Engelberg/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha7"]
-                 [better-cond "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha7"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :java-source-paths ["src/java"])
