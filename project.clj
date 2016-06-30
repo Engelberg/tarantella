ad330d54b4d08506e012e11c13c1beb63d955fbe
Mark Engelberg <mark.engelberg@gmail.com>
2016-06-29 18:04:24 -0700

    Added test stub

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha8"]
-                 [better-cond "1.0.1"]]
+                 [better-cond "1.0.1"]] 
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"])
---
+++ test/tarantella/core_test.clj
@@ -0,0 +1,8 @@
+(ns tarantella.core-test
+  (:require 
+    clojure.test
+    [tarantella.core :as t]))
+
+;; I use this library in my own work, but cannot open-source my tests.
+;; I would welcome pull requests with tests and examples.
+
