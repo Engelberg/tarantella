25a4977f3d7dab68cb93da50cdea79d948099786
Mark Engelberg <mark.engelberg@gmail.com>
2016-06-29 18:09:14 -0700

    Added comment to test stub

--- test/tarantella/core_test.clj
+++ test/tarantella/core_test.clj
@@ -6,3 +6,8 @@
 ;; I use this library in my own work, but cannot open-source my tests.
 ;; I would welcome pull requests with tests and examples.
 
+;; This would also be a good exercise for someone who wants to learn test.check
+;; to generate random matrices of 0s and 1s and check that the output is a valid covering.
+;; Checking the output would be straightforward for matrix inputs, 
+;; just recover the original rows from the row numbers, then
+;; (apply map + list-of-rows) and check that it is all 1's.
